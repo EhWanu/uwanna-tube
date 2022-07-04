b356845b1468c4a7f327cd3cdb08482db932caed
EhWanu <cameronjshadgett@gmail.com>
2022-07-04 15:22:23 -0400

    created components and basica styles layout

---
+++ src/components/Menu.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import styled from "styled-components"
+
+// STYLED COMPONENTS
+const Container = styled.div`
+  flex: 1;
+  background-color: #202020;
+  height: 100vh;
+  color: white;
+`
+const Wrapper = styled.div`
+  padding: 18px 26px;
+`
+
+const Menu = () => {
+  return <Container>Menu</Container>
+}
+
+export default Menu
